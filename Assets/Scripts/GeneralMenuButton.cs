81bb26e1759161624eed91f1bbab7d987c40c530
Diane <dianedu@nevada.unr.edu>
2021-12-05 14:01:38 -0800

    Got the menu buttons to work as inteneded. still need work on the implementation of functionalities and algorithms panel

---
+++ Assets/Scripts/GeneralMenuButton.cs
@@ -0,0 +1,33 @@
+//This script is for the general functionality of a menu dropdown 
+//When the program begins, the dropdown panels should not be shown or accessible
+//The dropdown appears when the user clicks on the menu button and remains until the user selects an option or clicks away
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class GeneralMenuButton : MonoBehaviour
+{
+    private GameObject panelObj;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //each menu option has a Panel child to activate and de-activate 
+        panelObj = transform.GetChild(1).gameObject;
+        //want the dropdown to not be shown or accessible when program first starts
+        panelObj.gameObject.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (EventSystem.current.currentSelectedGameObject == this.gameObject){
+            panelObj.gameObject.SetActive(true);
+        }
+        else{
+            panelObj.gameObject.SetActive(false);
+        }
+    }
+}
