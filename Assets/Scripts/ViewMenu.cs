40e964a409ae4996d22433c2d8895e376c7d9069
Jimson Huang <jimsonjy@gmail.com>
2021-12-07 05:42:17 -0800

    Implemented View->Show Label button

---
+++ Assets/Scripts/ViewMenu.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.EventSystems;
+
+public class ViewMenu : MonoBehaviour
+{
+    [SerializeField]
+    private TMP_Text showGrpahLabelsText;
+
+    private void Update()
+    {
+        // TODO: Avoid using EventSystem for UI updates
+        if (EventSystem.current.currentSelectedGameObject == showGrpahLabelsText.transform.parent.gameObject)
+        {
+            ToggleLabels();
+            EventSystem.current.SetSelectedGameObject(null);
+        }
+    }
+
+    // Method called by the "Show Graph Labels" button in the view dropdown to toggle the display of vertex and edge labels
+    public void ToggleLabels()
+    {
+        Debug.Log("Test");
+        if (Controller.singleton.displayVertexLabels)
+        {
+            Controller.singleton.displayVertexLabels = false;
+            showGrpahLabelsText.text = "Show Graph Labels";
+        }
+        else
+        {
+            Controller.singleton.displayVertexLabels = true;
+            showGrpahLabelsText.text = "Hide Graph Labels";
+        }
+    }
+}
