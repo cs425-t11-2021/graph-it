80a5fef11a2215fb81c88873a9d95b1c0393352b
Jimson Huang <jimsonjy@gmail.com>
2022-02-10 14:49:35 -0800

    Updated project sturcture 2

--- Assets/Scripts/Experimentation/Ref.cs
+++
@@ -1,9 +0,0 @@
-public class Ref<T>
-{
-    private T backing;
-    public T Value { get { return backing; } set { backing = value; } }
-    public Ref(T reference)
-    {
-        backing = reference;
-    }
-}
