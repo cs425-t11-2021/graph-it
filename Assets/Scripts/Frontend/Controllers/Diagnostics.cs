435a4e299220eb0d00620c409aab8d31897d95de
Jimson Huang <jimsonjy@gmail.com>
2022-02-19 00:59:50 -0800

    Added more functionality to Diagnostics

--- Assets/Scripts/Frontend/Controllers/Diagnostics.cs
+++ Assets/Scripts/Frontend/Controllers/Diagnostics.cs
@@ -8,7 +8,8 @@ public class Diagnostics : SingletonBehavior<Diagnostics>
     [SerializeField] private bool printMemoryLogs = false;
 
     private long previousSecondMemory = 0;
-    
+    private long tenSecondsAgoMemory = 0;
+
     private void Awake()
     {
         if (this.printMemoryLogs)
@@ -20,6 +21,7 @@ public class Diagnostics : SingletonBehavior<Diagnostics>
 
     private IEnumerator LogMemoryUse()
     {
+        int iteration = 0;
         while (true)
         {
             long memory = System.Diagnostics.Process.GetCurrentProcess().PrivateMemorySize64 / (1024*1024);
@@ -28,7 +30,18 @@ public class Diagnostics : SingletonBehavior<Diagnostics>
                 Logger.Log(String.Format("Process Memory: {0} mb, Rate: {1} mb/s", memory, memory - this.previousSecondMemory), this, LogType.INFO);
             }
 
+            if (iteration % 10 == 0)
+            {
+                if (this.tenSecondsAgoMemory != 0)
+                {
+                    Logger.Log(String.Format("Rate Over Last Ten Seconds: {0} mb/s", (memory - this.previousSecondMemory) / 10f), this, LogType.INFO);
+                }
+
+                this.tenSecondsAgoMemory = memory;
+            }
+
             this.previousSecondMemory = memory;
+            iteration++;
             yield return new WaitForSeconds(1);
         }
 
