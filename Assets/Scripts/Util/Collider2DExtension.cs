cb43596f4579b682467004f16b2d68ade862bab3
Jimson Huang <jimsonjy@gmail.com>
2022-03-04 23:04:27 -0800

    Changed ComponentExtension to Collider2DExtension

---
+++ Assets/Scripts/Util/Collider2DExtension.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Security.Permissions;
+using UnityEngine;
+
+public static class Collider2DExtension
+{
+    public static CircleCollider2D CopyFromCollider(this CircleCollider2D self, CircleCollider2D other)
+    {
+        self.radius = other.radius;
+        self.offset = other.offset;
+        return self;
+    }
+    
+    public static PolygonCollider2D CopyFromCollider(this PolygonCollider2D self, PolygonCollider2D other)
+    {
+        Vector2[] points = new Vector2[other.points.Length];
+        other.points.CopyTo(points, 0);
+        self.points = points;
+        self.offset = other.offset;
+        return self;
+    }
+}
