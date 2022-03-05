01242a601458ef6fda3510b2f368a1d2022a7d90
Jimson Huang <jimsonjy@gmail.com>
2022-03-04 22:47:12 -0800

    Added utility class with Copy Component functionality

---
+++ Assets/Scripts/Util/ComponentExtension.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ComponentExtension : MonoBehaviour
+{
+    // https://answers.unity.com/questions/458207/copy-a-component-at-runtime.html
+    public static T CopyComponent<T>(T original, GameObject destination) where T : Component
+    {
+        System.Type type = original.GetType();
+        var dst = destination.GetComponent(type) as T;
+        if (!dst) dst = destination.AddComponent(type) as T;
+        var fields = type.GetFields();
+        foreach (var field in fields)
+        {
+            if (field.IsStatic) continue;
+            field.SetValue(dst, field.GetValue(original));
+        }
+        var props = type.GetProperties();
+        foreach (var prop in props)
+        {
+            if (!prop.CanWrite || !prop.CanWrite || prop.Name == "name") continue;
+            prop.SetValue(dst, prop.GetValue(original, null), null);
+        }
+        return dst as T;
+    }
+}
