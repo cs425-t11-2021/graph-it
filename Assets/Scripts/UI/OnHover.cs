b03692bb23a5d46672fa2763f7fae9f5f42e4730
Diane <dianedu@nevada.unr.edu>
2022-03-19 18:30:18 -0700

    used the event trigger component, kind of getting to work but still not quite working, need to look into how to create and destroy prefabs

--- Assets/Scripts/UI/OnHover.cs
+++ Assets/Scripts/UI/OnHover.cs
@@ -2,22 +2,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.EventSystems;
+//using UnityEngine.EventSystems;
 
-public class OnHover : EventTrigger
+public class OnHover : MonoBehaviour
 {
     //Prefab of UI label on hover
     [SerializeField] private GameObject labelPrefab;
-    // Start is called before the first frame update
-   public override void OnPointerEnter(PointerEventData eventData)
-    {
-        OnHoverLabel newLabel = Instantiate(labelPrefab, this.transform).GetComponent<OnHoverLabel>();
-        newLabel.InitiateLabel(this.transform.parent.gameObject);
+    
+   public void OnHoverMouse(){
+        //OnHoverLabel newLabel = Instantiate(labelPrefab).GetComponent<OnHoverLabel>();
+        //newLabel.CreateLabel(this.transform.parent.gameObject);
         Debug.Log("On pointer enter called");
-      
-    }
+   }
 
-    public override void OnPointerExit(PointerEventData eventData)
+    public void OnHoverExit()
     {
         Debug.Log("On pointer exit called");
      
--- Assets/Scripts/UI/OnHoverLabel.cs
+++ Assets/Scripts/UI/OnHoverLabel.cs
@@ -6,8 +6,7 @@ using UnityEngine;
 
 public class OnHoverLabel : MonoBehaviour
 {
-    //Prefab of UI label on hover
-    [SerializeField] private GameObject labelPrefab;
+    
     private TMP_Text label;
     //the text component of the label when the user hovers over the UI element
 
@@ -18,7 +17,6 @@ public class OnHoverLabel : MonoBehaviour
     }
 
     public void CreateLabel(GameObject parentObject){
-        this.Instantiate(labelPrefab).GetComponent<OnHoverLabel>();
         this.label.SetText(parentObject.GetComponent<TMP_Text>().text);
     }
 
